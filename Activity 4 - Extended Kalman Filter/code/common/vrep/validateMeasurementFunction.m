0fb7e5db5da50ff5b1a8acd3ae0c6765e4b46f24
ito.rafael <ito.rafael@gmail.com>
2017-11-19 17:31:44 -0200

    ex.4: update step --> measure function added

--- Activity 4 - Extended Kalman Filter/code/common/vrep/validateMeasurementFunction.m
+++ Activity 4 - Extended Kalman Filter/code/common/vrep/validateMeasurementFunction.m
@@ -6,11 +6,11 @@ success = 1;
 load data/validationData.mat
 
 try
-    
+
     for i = 1:length(measurementData)
         for j = 1:size(measurementData(i).M, 2)
             [h, Hx] = measurementFunction(measurementData(i).x, measurementData(i).M(:,j));
-            
+
             if any(abs(h - measurementData(i).h(:,j)) > tolerance) || ...
                     any(reshape(abs(Hx - measurementData(i).Hx(:,:,j)), [], 1) > tolerance)
                 success = 0;
@@ -19,11 +19,11 @@ try
             end
         end
     end
-    
+
     if success == 1
         fprintf('measurement function appears to be correct!\n');
     end
-    
+
 catch
     fprintf('Error in the user supplied function. Please make sure that your function is syntactically correct and adheres to the conventions described in the exercise!\n')
     success = 0;
